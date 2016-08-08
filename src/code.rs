7f40039d6b30d259c07a57f9a6a36275ecc33096
Markus Becker <mtb@tibyte.net>
2016-08-08 18:39:54 +0200

    split testing

--- src/code.rs
+++ src/code.rs
@@ -41,3 +41,41 @@ pub fn trim(data: &[u8]) -> (usize, usize) {
         }
     )
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fmt;
+
+    const TO_TEST: [(&'static str, &'static str, &'static str); 4] = [
+        ("800 Chat message", "800", "Chat message"),
+        ("200 Login", "200", "Login"),
+        ("201 Username", "201", "Username"),
+        ("code content content content", "cod", " content content content"),
+    ];
+
+    type Splitter<'a, A> = Fn(A) -> Option<(&'a str, &'a str)>;
+
+    #[test]
+    fn test_split_str() {
+        check_split_iter(TO_TEST.to_vec(), &split_str);
+    }
+
+    #[test]
+    fn test_split_u8() {
+        let to_test_iter = TO_TEST.iter().map(|&(x,y,z)| (x.as_bytes(),y,z)).collect::<Vec<(&[u8], &str, &str)>>();
+        check_split_iter(to_test_iter, &split_u8);
+    }
+
+    fn check_split_iter<T: fmt::Debug + Copy>(i: Vec<(T, &str, &str)>, s: &Splitter<T>) {
+        let mut it = i.iter();
+        while let Some(&(t, code, content)) = it.next() {
+            if let Some((res_code, res_content)) = s(t) {
+                assert_eq!(res_code, code);
+                assert_eq!(res_content, content);
+            } else {
+                panic!("Failed testing {:?}, expected {}:{}", t, code, content);
+            }
+        }
+    }
+}

93cb08835a7b3a0ba17214ba9ffe2c8f322c6448
LFalch <lucas@wasd.dk>
2016-08-08 15:43:26 +0200

    Remove unnecessary explicit lifetimes

--- src/code.rs
+++ src/code.rs
@@ -1,7 +1,7 @@
 use std::str;
 
 /// Split a message into <code> and <content>
-pub fn split_str<'a>(msg: &'a str) -> Option<(&'a str, &'a str)> {
+pub fn split_str(msg: &str) -> Option<(&str, &str)> {
     if msg.len() > 4 {
         Some((&msg[..3], &msg[4..]))
     } else {
@@ -10,7 +10,7 @@ pub fn split_str<'a>(msg: &'a str) -> Option<(&'a str, &'a str)> {
 }
 
 /// Split a message into <code> and <content>
-pub fn split_u8<'a>(data: &'a [u8]) -> Option<(&'a str, &'a str)> {
+pub fn split_u8(data: &[u8]) -> Option<(&str, &str)> {
     match str::from_utf8(data) {
         Ok(s) => split_str(s),
         Err(_) => None,
